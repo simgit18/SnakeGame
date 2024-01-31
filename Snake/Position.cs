24733ab62c9ab08aae540a140e583da0cdf59dec
Shivam Patel <shivamp1804@gmail.com>
2024-02-01 00:39:28 +0530

    basic classes created and methods added

---
+++ Snake/Position.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Position
+    {
+        public int Row;
+        public int Col;
+        
+        public Position(int row, int col) {  Row = row; Col = col; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Position position &&
+                   Row == position.Row &&
+                   Col == position.Col;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+
+        public Position Translate(Direction dir)
+        {
+            return new Position(Row + dir.RowOffset, Col + dir.ColOffset);
+        }
+
+        public static bool operator ==(Position? left, Position? right)
+        {
+            return EqualityComparer<Position>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Position? left, Position? right)
+        {
+            return !(left == right);
+        }
+    }
+}
